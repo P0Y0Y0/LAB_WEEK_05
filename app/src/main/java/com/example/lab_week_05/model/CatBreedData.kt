6162aaa39709d66989826c94b2fa1815ce5f2a02
P0Y0Y0 <erufumang@gmail.com>
2025-09-29 19:19:20 +0700

    Commit No. 2 - Parse JSON with Moshi and Display Image URL

---
+++ app/src/main/java/com/example/lab_week_05/model/CatBreedData.kt
@@ -0,0 +1,8 @@
+package com.example.lab_week_05.model
+
+import android.health.connect.datatypes.units.Temperature
+
+data class CatBreedData (
+    val name: String,
+    val temperature: String
+)
